017e171560a1fd7fbdce479f59f1b2d037234d07
Paulo Morgado <470455+paulomorgado@users.noreply.github.com>
2021-07-04 00:01:42 +0100

    moved host building and initialiation to HTTP module

---
+++ src/Microsoft.AspNet.Hosting.HttpRuntime/WebHostStartupAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Microsoft.AspNet.Hosting
+{
+    /// <summary>
+    /// Used to mark which class in an assembly should be the startup type to be used by the web host.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public sealed class WebHostStartupAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebHostStartupAttribute"/> class
+        /// </summary>
+        /// <param name="startupType">The type of the startup class.</param>
+        public WebHostStartupAttribute(Type startupType)
+        {
+            StartupType = startupType;
+        }
+
+        /// <summary>
+        /// Gets the type of the startup class.
+        /// </summary>
+        /// <value>The type of the startup class.</value>
+        public Type StartupType { get; }
+    }
+}
